0a3ba3cba51aefb833e4b123feaa72c5197b97ca
Khaled Al Nabaheen <khalednabaheen8@gmail.com>
2025-01-13 21:35:56 +0200

    Backend - UPost Server Created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import { PORT } from './config/index.js';
 
+const app = express();
+app.set('port', PORT || 3000);
+
+app.disable('x-powered-by');
+
+app.use([express.json(), express.urlencoded({ extended: false }), cookieParser(), compression()]);
+
+app.get('/', (req, res) => {
+  res.json({ msg: '🚀 Hello World!' });
+});
+
+export default app;
